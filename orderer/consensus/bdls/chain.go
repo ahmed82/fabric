64298b12546858fceccd16a34cfe7bbecc7d178e
Ahmed Al-Salih <a.alsalih2@gmail.com>
2023-03-08 22:15:11 -0500

    fix RPC interface implementation reusing  the comentted error fun

--- orderer/consensus/bdls/chain.go
+++ orderer/consensus/bdls/chain.go
@@ -53,7 +53,7 @@ const None uint64 = 0
 // RPC is used to mock the transport layer in tests.
 type RPC interface {
 	SendConsensus(dest uint64, msg *orderer.ConsensusRequest) error
-	SendSubmit(dest uint64, request *orderer.SubmitRequest /* report func(err error)*/) error
+	SendSubmit(dest uint64, request *orderer.SubmitRequest, report func(err error)) error
 }
 
 //go:generate counterfeiter -o mocks/mock_blockpuller.go . BlockPuller
